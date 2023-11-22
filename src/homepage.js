58f26e1dee5c725ea25ff6b6b2effe98732bcf8a
rich <dankwarrior6900@gmail.com>
2023-11-22 08:52:59 -0800

    Edit description in home page to make the string more readable

--- src/homepage.js
+++ src/homepage.js
@@ -20,13 +20,19 @@ div.appendChild(h1);
 
 const description = document.createElement('p');
 description.classList.add('bannerDesc');
-description.textContent = 'Choso is an incarnated cursed womb: Death Paintings Number 1 - 3. He initially sided with Kenjaku and the disaster curses. However, after finding out about the identity of the Death Paintings father, Noritoshi Kamo, Choso decided to switch alliances and side with Yuji.'
+description.textContent = `Choso is an incarnated cursed womb: Death Paintings Number 1 - 3.
+                             He initially sided with Kenjaku and the disaster curses. However, 
+                             after finding out about the identity of the Death Paintings father, 
+                             Noritoshi Kamo, Choso decided to switch alliances and side with Yuji.`;
 
 div.appendChild(description);
 
 const stats = document.createElement('p');
 stats.classList.add('bannerDesc');
-stats.innerHTML = '<p> Species: Death Painting Wombs <br> Age: 150+ <br> Gender: Male <br> Height: 181cm (5-11) <br> Hair Color: Black <br> Eye Color: Purple</p>';
+stats.innerHTML = `<p> Species: Death Painting Wombs <br> 
+                    Age: 150+ <br> Gender: Male <br> Height: 
+                    181cm (5-11) <br> Hair Color: Black <br> 
+                    Eye Color: Purple</p>`;
 div.appendChild(stats);
 }
 
