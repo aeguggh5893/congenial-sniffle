2ae50c368c4496d0337744a73e355f912be523a2
rich <dankwarrior6900@gmail.com>
2023-11-22 15:05:20 -0800

    Finish techniques page

--- src/moveset.js
+++ src/moveset.js
@@ -14,6 +14,7 @@ export function moveset() {
     container3.appendChild(banner);
 
     const description = document.createElement('p');
+    description.classList.add('moveset-info');
     description.textContent = `Choso is ranked as a special grade. He is exceptionally powerful even by cursed spirit standards
                                 Choso was intelligent and self-aware which allowed him to hone his cursed techniques for 150 years.
                                 By the time he was incarnated, Choso had already mastered Blood Manipulation to a level that far
@@ -26,36 +27,49 @@ export function moveset() {
     techTitle.textContent = 'Techniques';
     container3.appendChild(techTitle);
 
+    // const techDiv = document.createElement('div');
+    // techDiv.classList.add('techniques-section');
+    // container3.appendChild(techDiv);
+
+    // const img = document.createElement('img');
+    // img.src = '../src/assets/choso-pb.webp';
+    // img.alt = 'Choso using piercing blood';
+    // img.classList.add('tech-image');
+    // techDiv.appendChild(img);
+
+    // const tech1 = document.createElement('p');
+    // tech1.innerText = 'Choso using piercing blood utilizing convergence';
+    // techDiv.appendChild(tech1);
+
+    let pictures = ['choso-pb', 'choso-supernova', 'choso-se'];
+
     const techDiv = document.createElement('div');
     techDiv.classList.add('techniques-section');
     container3.appendChild(techDiv);
 
+    for (let i = 0; i < 3; i++) {
     const img = document.createElement('img');
-    img.src = '../src/assets/choso-pb.webp';
-    img.alt = 'Choso using piercing blood';
+    img.src = `../src/assets/${pictures[i]}.webp`;
+    img.alt = `Choso using his curse techniques`;
     img.classList.add('tech-image');
     techDiv.appendChild(img);
 
-    const tech1 = document.createElement('p');
-    tech1.innerText = 'Choso using piercing blood utilizing convergence';
-    techDiv.appendChild(tech1);
+    const tech = document.createElement('p');
+    tech.classList.add('tech-desc');
+    
+    if (i === 0) {
+        tech.innerHTML = `<strong><em>Piercing Blood</em></strong> - A deadly blood beam fired from Choso's clasped hands after charging up Convergence. Choso is able to bend the beam and make it ricochet should it miss, 
+        but Piercing Blood does not maintain the same level of extreme velocity as when its first shot.`
+    } else if (i === 1) {
+        tech.innerHTML = `<strong><em>Supernova</em></strong> - A Choso original that he developed while honing Blood Manipulation for 150 years. 
+        It detonates several orbs of blood condensed with Convergence and fires reinforced blood bullets in every direction. It can also be channeled into one larger sphere that explodes into an even more devastating attack.`
+    } else {
+        tech.innerHTML = `<strong><em>Slicing Exorcism</em></strong> - Unleashes sharpened blood that rapidly flies through the air and slices through Choso's target. He is capable of shaping it into long streams of blood at a wide range for hitting or forming it into a rotating charkarm for a singular attack`
+    }
 
-    const img2 = document.createElement('img');
-    img2.src = '../src/assets/choso-supernova.webp';
-    img2.alt = 'Choso using piercing blood';
-    img2.classList.add('tech-image');
-    techDiv.appendChild(img2);
-
-    const tech2 = document.createElement('p');
-    tech2.innerText = 'Choso using piercing blood utilizing convergence';
-    techDiv.appendChild(tech2);
+    techDiv.appendChild(tech);
 }
-
-/*
-
-
-function displayTechnique() {
 
 }
 
-*/
+
